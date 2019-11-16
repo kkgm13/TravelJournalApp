1ba1a5864f0303e523000494e156fa60d41a33ac
Kel Ming <kkgm013@gmail.com>
2019-11-16 16:21:58 +0000

    Holiday Create Page now working correctly

--- app/src/main/java/com/kkgmdevelopments/traveljournalapp/HolidayActivity.java
+++ app/src/main/java/com/kkgmdevelopments/traveljournalapp/HolidayActivity.java
@@ -1,13 +1,16 @@
 package com.kkgmdevelopments.traveljournalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HolidayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.holiday_create);
     }
 }
--- app/src/main/java/com/kkgmdevelopments/traveljournalapp/MainActivity.java
+++ app/src/main/java/com/kkgmdevelopments/traveljournalapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kkgmdevelopments.traveljournalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -22,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(MainActivity.this, HolidayActivity.class);
+                startActivity(intent);
             }
         });
     }

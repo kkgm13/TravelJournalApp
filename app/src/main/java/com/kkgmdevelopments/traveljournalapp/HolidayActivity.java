c741998d5e55079e8660929c8715482a63915aba
Kel Ming <kkgm013@gmail.com>
2019-11-27 14:41:52 +0000

    Working Date Picker Dialogs. Need to pass min value from starting position

--- app/src/main/java/com/kkgmdevelopments/traveljournalapp/HolidayActivity.java
+++ app/src/main/java/com/kkgmdevelopments/traveljournalapp/HolidayActivity.java
@@ -3,12 +3,20 @@ package com.kkgmdevelopments.traveljournalapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HolidayActivity extends AppCompatActivity {
 
@@ -16,8 +24,10 @@ public class HolidayActivity extends AppCompatActivity {
             "com.kkgmdevelopments.traveljournalapp.roomholiday.REPLY";
 
     private EditText mCreateHolidayName;
-    private EditText mCreateHolidayStartDate;
-    private EditText mCreateHolidayEndDate;
+    private TextView mCreateHolidayStartDate;
+    private DatePickerDialog.OnDateSetListener mHolidayStartDate;
+    private TextView mCreateHolidayEndDate;
+    private DatePickerDialog.OnDateSetListener mHolidayEndDate;
     private EditText mCreateHolidayNotes;
 
     @Override
@@ -30,6 +40,53 @@ public class HolidayActivity extends AppCompatActivity {
         mCreateHolidayStartDate = findViewById(R.id.holiday_start_date);
         mCreateHolidayEndDate = findViewById(R.id.holiday_end_date);
         mCreateHolidayNotes = findViewById(R.id.holiday_notes);
+
+        mCreateHolidayStartDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(HolidayActivity.this, R.style.DialogTheme, mHolidayStartDate ,year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+
+        mHolidayStartDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mCreateHolidayStartDate.setText(date);
+            }
+        };
+
+        mCreateHolidayEndDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(HolidayActivity.this, R.style.DialogTheme, mHolidayEndDate ,year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+//                dialog.getDatePicker().setMinDate();
+                dialog.show();
+            }
+        });
+
+        mHolidayEndDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mCreateHolidayEndDate.setText(date);
+            }
+        };
 
         //Save button
         final Button button = findViewById(R.id.holiday_save);
